0472e17f6c70630391d4528a3bd7624d8d57dcc9
Thomas4410 <62375263+Thomas4410@users.noreply.github.com>
2020-06-26 09:44:35 +0200

    Create formulaire.php

---
+++ formulaire.php
@@ -0,0 +1,24 @@
+
+
+<p>Pour finaliser votre inscription, merci de renseigner vos informations</p>
+
+<form action="/ma-page-de-traitement" method="post">
+    <div>
+        <label for="name">Nom :</label>
+        <input type="text" id="name" name="user_name">
+    </div>
+    <div>
+        <label for="city">Ville :</label>
+        <input type="text" id="city" name="user_city">
+    </div>
+    <div>
+        <label for="phone">Numéro de téléphone :</label>
+        <input type="text" id="phone" name="user_phone">
+    </div>
+    <div>
+        <label for="mail">e-mail :</label>
+        <input type="text" id="mail" name="user_mail">
+    </div>
+</form>
+
+<p>Vos informations ont bien été enregistrées</p>
